c0b7e96e63d0da300b273c67dd1a93e277193e2c
navidaziz98 <navidaziz98@gmail.com>
2023-02-22 13:16:33 +0500

    some chagen

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -35,13 +35,11 @@ class MY_Controller extends CI_Controller
     public function upload_file($field_name, $config = NULL)
     {
 
-        echo $field_name;
-        exit();
 
         if (is_null($config)) {
             $config = array(
                 "upload_path" => "./assets/uploads/" . $this->router->fetch_class() . "/",
-                "allowed_types" => "jpg|jpeg|bmp|png|gif|doc|docx|xlsx|xls|pdf|ppt|pptx|webp|mp4|wmp|WMP",
+                "allowed_types" => "jpg|jpeg|bmp|png|gif|doc|docx|xlsx|xls|pdf|ppt|pptx|webp|mp4|wmv|WMV",
                 "max_size" => 1024 * 100,
                 "max_width" => 0,
                 "max_height" => 0,
